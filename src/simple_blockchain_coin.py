f4591a331c989f015e37d2faea6c86915379e794
gianluca.gueli <gianluca.gue99@gmail.com>
2024-08-01 19:36:55 +0200

    update README

--- src/simple_blockchain_coin.py
+++ src/simple_blockchain_coin.py
@@ -1,3 +1,21 @@
+# Simple blockchain program
+# The project was created by following this youtube tutorial: https://www.youtube.com/watch?v=pYasYyjByKI
+#
+# example
+# | Transaction | From   | To     | Amount    |
+# |-------------|--------|--------|-----------|
+# | t1          | Anna   | Andres | 2 SBCC    |
+# | t2          | Bob    | Joe    | 4.3 SBCC  |
+# | t3          | Mark   | Mark   | 3.2 SBCC  |
+# | t4          | Andres | Mark   | 0.15 SBCC |
+# | t5          | Mark   | Miguel | 1.25 SBCC |
+# | t6          | Joe    | Mark   | 0.77 SBCC |
+# 
+#
+# BLOCK1 ("FirstBlock", t1, t2) --hash--> 76fd89
+# BLOCK2 ("76fd89", t3, t4) --hash--> 83jan3
+# BLOCK3 ("83jan3", t5, t6) --hash--> 6782ff
+
 import hashlib
 
 
